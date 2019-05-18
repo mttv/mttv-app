7bc27a6617808231794d54c4a6c993ec7ab92e6d
Unknown <sergeitchaban@gmail.com>
2019-05-18 12:14:01 +0300

    Clearing rep branch

--- app/electron/extensions/bttv/betterttv.js
+++
@@ -1,8 +0,0 @@
-(function betterttv() {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://cdn.betterttv.net/betterttv.js';
-    var head = document.getElementsByTagName('head')[0];
-    if (!head) return;
-    head.appendChild(script);
-})()
